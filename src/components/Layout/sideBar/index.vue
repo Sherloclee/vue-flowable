8d9b08fc280d0d83319416afc8ec56b7ab3c3322
sherloclee <sherloc_lee@163.com>
2020-08-03 12:00:09 +0800

    menu router-link

--- src/components/Layout/sideBar/index.vue
+++ src/components/Layout/sideBar/index.vue
@@ -1,28 +1,32 @@
 <template>
-      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+    <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
         <a-sub-menu>
-          <span slot="title">菜单123</span>
-          <a-menu-item key="1">
-            <a-icon type="user" />
-            <span>nav 1</span>
-          </a-menu-item>
+            <span slot="title">
+                <a-icon type="mail" />
+                <span>菜单123</span>
+            </span>
+            <a-menu-item key="1">
+                <router-link to="/helloworld">
+                    <a-icon type="user" />
+                    <span>nav 1</span>
+                </router-link>
+            </a-menu-item>
         </a-sub-menu>
         <a-menu-item key="2">
-          <a-icon type="video-camera" />
-          <span>nav 2</span>
+            <router-link to="/workflow/bpmn">
+                <a-icon type="video-camera" />
+                <span>nav 2</span>
+            </router-link>
         </a-menu-item>
         <a-menu-item key="3">
-          <a-icon type="upload" />
-          <span>nav 3</span>
+            <a-icon type="upload" />
+            <span>nav 3</span>
         </a-menu-item>
-      </a-menu>
+    </a-menu>
 </template>
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
